39e809b90ddf80f1dd60f66ed6f682e7ab938e7b
PowerHat <b.raymond@protonmail.com>
2018-10-28 00:40:51 +0200

    Update todo annotations

--- src/utils.py
+++ src/utils.py
@@ -2,10 +2,27 @@ from src.gradients import compute_logistic_gradient
 
 
 def logistic_gradient_step(y, tx, w, gamma):
+    """
+    @TODO move this (logistic helper)
+    :param y:
+    :param tx:
+    :param w:
+    :param gamma:
+    :return:
+    """
     grad = compute_logistic_gradient(y, tx, w)
     return w - gamma * grad
 
 
 def reg_logistic_gradient_step(y, tx, w, l, gamma):
+    """
+    @TODO move this (logistic helper)
+    :param y:
+    :param tx:
+    :param w:
+    :param l:
+    :param gamma:
+    :return:
+    """
     grad = compute_logistic_gradient(y, tx, w) + 2 * l * w
     return w - gamma * grad
