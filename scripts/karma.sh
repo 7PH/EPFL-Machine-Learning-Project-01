fe0e312bb07c18327d2e6ebdb98475a1f36a32ee
PowerHat <b.raymond@protonmail.com>
2018-10-28 00:50:38 +0200

    Update test script

--- scripts/karma.sh
+++ scripts/karma.sh
@@ -1,6 +1,21 @@
 #!/usr/bin/env bash
 
-(ls data/test.csv data/train.csv &&
-    echo "Everything seems fine") ||
-(echo "\n/!\\ CSV files are missing /!\\ \n" &&
-    exit 1);
+RED='\033[0;31m'
+NC='\033[0m'
+
+ensure_command_exists() {
+    command -v $1 >/dev/null 2>&1 || { echo >&2 "$RED I require $1 but it's not installed. $NC"; exit 1; }
+}
+
+ensure_file_exit() {
+    ls $1 || (echo "$RED/!\\ File $1 is missing /!\\ $NC" && exit 1);
+}
+
+echo "\nVerifying that csv files are present..";
+ensure_file_exit "data/test.csv";
+ensure_file_exit "data/train.csv";
+
+echo "\nVerifying that python linter is installed..";
+ensure_command_exists "pylint";
+
+echo "\nEverything seems OK";
