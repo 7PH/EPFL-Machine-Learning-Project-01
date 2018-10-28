65784641da552efc0baed3f50a3c31dea41d570f
PowerHat <b.raymond@protonmail.com>
2018-10-29 00:44:53 +0100

    Finish unit tests

--- test/implementations.py
+++ test/implementations.py
@@ -2,13 +2,13 @@ import unittest
 
 import numpy as np
 
-from src.helpers import load_csv_data
+from src.helpers import load_csv_data, predict_labels, label_accuracy
 from src.implementations import least_squares_GD, least_squares_SGD, least_squares, logistic_regression, \
     reg_logistic_regression
 
 DATA_FOLDER = './data/'
 
-y, tx, x_ids = load_csv_data(DATA_FOLDER + "train.csv")
+y, tx, _ = load_csv_data(DATA_FOLDER + "train.csv")
 y = y.reshape((len(y), 1))
 
 
@@ -20,29 +20,31 @@ class TestImplementations(unittest.TestCase):
         self.gamma = 0.1
         self.lambda_ = 0.001
 
-    def assert_result(self, w, loss):
-        self.assertEqual(1, 1)
+    def assert_result(self, name, w, loss):
+        pred = predict_labels(w, tx)
+        acc = label_accuracy(pred, y)
+        self.assertGreater(acc, 60, msg="Accuracy for " + name + " is suspicious")
 
     def test_least_squares_gd(self):
         w, loss = least_squares_GD(y, tx, self.initial_w, self.max_iters, self.gamma)
-        self.assert_result(w, loss)
+        self.assert_result('least_squares_gd', w, loss)
 
     def test_least_squares_sgd(self):
         w, loss = least_squares_SGD(y, tx, self.initial_w, self.max_iters, self.gamma)
-        self.assert_result(w, loss)
+        self.assert_result('least_squares_sgc', w, loss)
 
     def test_least_squares(self):
         w, loss = least_squares(y, tx)
-        self.assert_result(w, loss)
+        self.assert_result('lest_squares', w, loss)
 
     def test_ridge_regression(self):
         w, loss = least_squares(y, tx)
-        self.assert_result(w, loss)
+        self.assert_result('ridge', w, loss)
 
     def test_logistic_regression(self):
         w, loss = logistic_regression(y, tx, self.initial_w, self.max_iters, self.gamma)
-        self.assert_result(w, loss)
+        self.assert_result('logistic', w, loss)
 
     def test_reg_logistic_regression(self):
         w, loss = reg_logistic_regression(y, tx, self.lambda_, self.initial_w, self.max_iters, self.gamma)
-        self.assert_result(w, loss)
+        self.assert_result('reg_logistic', w, loss)
