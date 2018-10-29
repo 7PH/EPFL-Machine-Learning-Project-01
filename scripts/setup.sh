4b29682667ffaf26d2c3de70a08b0a0d0c33dec6
PowerHat <b.raymond@protonmail.com>
2018-10-29 11:11:47 +0100

    Fix setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,5 +3,6 @@
 TEST_URL="http://uploads.benjamin-raymond.pro/2018/10/28/21-40-01-000-test.csv";
 TRAIN_URL="http://uploads.benjamin-raymond.pro/2018/10/28/21-40-01-001-train.csv";
 
+mkdir data;
 wget "$TEST_URL" -O data/test.csv;
 wget "$TRAIN_URL" -O data/train.csv;
